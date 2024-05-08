02b928db5f4253d2b90463ac8f5b3542714afd0c
Brodie Hill <brodiehill79@gmail.com>
2024-05-08 12:41:41 +1200

    Prep boat for AI

---
+++ Mini-Production1/Assets/SteeringWheel.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class SteeringWheel : MonoBehaviour
+{
+    [SerializeField]
+    private Transform wheel = null;
+
+    [SerializeField]
+    private float wheelRotationLimit = 600f;
+
+    [field: SerializeField]
+    public float SteerSpeed { get; set; } = 2f;
+
+    [field: SerializeField]
+    public bool Recenter { get; set; } = false;
+
+    [SerializeField]
+    private UnityEvent<float> onSteeringChanged;
+
+
+    public float CurrentSteerAngle { get; private set; }
+
+
+    private void LateUpdate()
+    {
+        float input = InputInstance.Controls.Boat.Steering.ReadValue<float>() * Time.deltaTime;
+
+        if (Mathf.Abs(input) > 0.001f)
+            CurrentSteerAngle += input * SteerSpeed;
+        else if (Recenter)
+            CurrentSteerAngle = Mathf.Lerp(CurrentSteerAngle, 0f, Time.deltaTime * SteerSpeed);
+
+        CurrentSteerAngle = Mathf.Clamp(CurrentSteerAngle, -1f, 1f);
+
+        if (wheel)
+            wheel.localRotation = Quaternion.Euler(0, 0, CurrentSteerAngle * wheelRotationLimit);
+
+        onSteeringChanged?.Invoke(CurrentSteerAngle);
+    }
+}
