9aa81ebc4ea07d3c9e794bb5acf6d50968edb2ce
Disgusting <disgustings@gmail.com>
2024-05-08 18:30:43 +1200

    scene changes! dont override!

--- Mini-Production1/Assets/SewerTurn.cs
+++ Mini-Production1/Assets/SewerTurn.cs
@@ -12,14 +12,8 @@ public class SewerTurn : MonoBehaviour
             triggered = true;
             //sewer.transform.localRotation = Quaternion.Euler(0,-90+rotationWalk,0f);
             var boat = GameManager.Instance.boat;
-            boat.GetComponent<BoatMovement>().originalRotation = Quaternion.Euler(boat.localRotation.x, boat.localRotation.y+45f, boat.localRotation.z);
+            //boat.GetComponent<BoatMovement>().originalRotation = Quaternion.Euler(boat.localRotation.x, boat.localRotation.y+45f, boat.localRotation.z);
             
-            //var cam = Camera.main.transform;
-            //cam.localRotation = Quaternion.Euler(cam.localRotation.x, cam.localRotation.y+45f, cam.localRotation.z);
-            // rotate the level
-            //transform.parent.rotation = Quaternion.Euler(transform.parent.rotation.x, transform.parent.localRotation.y,
-                //transform.parent.rotation.z);
-            //parent.transform.Rotate(new Vector3(0f,-45,0f));
             transform.parent.RotateAround(transform.position, new Vector3(0,1,0), -45);
         }
     }
