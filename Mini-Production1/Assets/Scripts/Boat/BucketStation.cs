23e97e8799f6fa2dc4387491d33a3da003d691f0
Brodie Hill <brodiehill79@gmail.com>
2024-05-15 18:15:11 +1200

    Add bucket bailing animation

--- Mini-Production1/Assets/Scripts/Boat/BucketStation.cs
+++ Mini-Production1/Assets/Scripts/Boat/BucketStation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class BucketStation : MonoBehaviour
 {
     [SerializeField]
@@ -10,9 +11,33 @@ public class BucketStation : MonoBehaviour
     [SerializeField, Tooltip("Health gain per second.")]
     private float bailRate = 0.4f;
 
+    [SerializeField]
+    private ParticleSystem bailParticles;
+
+    private Animator animator;
+    private int animBailHash;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        animBailHash = Animator.StringToHash("Bailing");
+    }
+
     private void Update()
     {
         Debug.Log("Chilly");
         boat.TakeDamage(-bailRate * Time.deltaTime);
+
+        //TODO maybe check boat max health and disable this when no more bailing to do?
+    }
+
+    public void AnimSetBail(bool val)
+    {
+        animator.SetBool(animBailHash, val);
+    }
+
+    public void Bail()
+    {
+        bailParticles.Play();
     }
 }
