12f7d8a0c06aff507d19ad4b2c36a1a7e6217169
Brodie Hill <brodiehill79@gmail.com>
2024-05-22 09:51:04 +1200

    Fix drain pipe damage

---
+++ Mini-Production1/Assets/Scripts/Hazards/DrainPipeHazard.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrainPipeHazard : Hazard
+{
+    [SerializeField]
+    private float damagePerSecond = 0.15f;
+
+    public override void OnImpacting(HazardImpactor hazardImpactor)
+    {
+        if (hazardImpactor.TryGetComponent(out BoatMovement boat))
+        {
+            boat.TakeDamage(damagePerSecond * Time.deltaTime);
+        }
+    }
+}
